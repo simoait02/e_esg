ff7466787306b926f1e2980b21cd9d1a2bce0bde
unknown <theofil888@gmail.com>
2024-07-07 20:59:39 +0100

    IeS

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:e_esg/pages/IES/ajoutLive.dart';
 import 'package:e_esg/pages/IES/calendrier.dart';
 import 'package:e_esg/pages/espaceMedecin/Card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-void main(){
-  runApp(Calendrier());
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calendrier(),
+    );
+  }
 }
