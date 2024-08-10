28cf1ebe815b3bd4f56287e7b9252e61f86b4487
mah <simoaithssaine09@gmail.com>
2024-08-10 14:57:15 +0100

    Tele Expertise+mon Profile+Chatbot version final

--- lib/pages/espaceMedecin/home/chatbot.dart
+++ lib/pages/espaceMedecin/home/chatbot.dart
@@ -1,237 +1,160 @@
-import 'package:e_esg/pages/espaceMedecin/LoginSignUp/Cardi.dart';
+import 'package:e_esg/api/end_points.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../LoginSignUp/Cardi.dart';
 
 class Chatbot extends StatefulWidget {
-  const Chatbot({super.key});
+  const Chatbot({Key? key}) : super(key: key);
+
   @override
-  ChatbotState createState() => ChatbotState();
+  State<Chatbot> createState() => _ChatbotState();
 }
 
-class ChatbotState extends State<Chatbot> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final TextEditingController controller = TextEditingController();
-  final List<Map<String, String>> messages = [];
-  int selectedIndex = 0;
+class _ChatbotState extends State<Chatbot> {
+  TextEditingController controller = TextEditingController();
+  List<String> messages = [];  // Ensure this is a list of strings
 
-  void sendMessage() {
-  if (controller.text.isNotEmpty) {
-    String userMessage = controller.text;
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+  }
+
+  Future<void> _loadMessages() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? id=prefs.getInt("IdDoc");
     setState(() {
-      messages.add({"type": "user", "text": userMessage});
-    });
-    String url = 'http://192.168.1.13:5000/Chatbot/ask';
-    http.post(Uri.parse(url), body: {'message': userMessage}).then((response) {
-      if (response.statusCode == 200) {
-        var responseData = jsonDecode(response.body);
-        setState(() {
-          messages.add({"type": "bot", "text": responseData['response']});
-        });
-      } else {
-        setState(() {
-          messages.add({"type": "bot", "text": "Error communicating with the bot"});
-        });
-      }
-      controller.clear();
-    }).catchError((error) {
-      setState(() {
-        messages.add({"type": "bot", "text": "Error: $error"});
-      });
-      controller.clear();
+      messages = prefs.getStringList('${id}_messages') ?? [];
     });
   }
-}
 
-  void resendMessage(String messageText) {
-  setState(() {
-    var lastBotMessage;
-    for (var i = messages.length - 1; i >= 0; i--) {
-      if (messages[i]['type'] == 'bot' && messages[i]['text'] != null) {
-        lastBotMessage = messages[i];
-        break;
-      }
-    }
-    if (lastBotMessage != null && lastBotMessage['text'] != null) {
-      messages.add({"type": "bot", "text": lastBotMessage['text']});
-    } else {
-      print('No previous bot message found with non-null text.');
-    }
-  });
-}
+  Future<void> _saveMessages() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? id=prefs.getInt("IdDoc");
+    await prefs.setStringList('${id}_messages', messages);
+  }
 
-  Widget buildMessage(Map<String, String> message, double screenWidth) {
-    bool isUser = message['type'] == 'user';
-    String messageText = message['text'] ?? '';
-
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        if (!isUser)
-          CircleAvatar(
-            backgroundImage: AssetImage('assets/images/esj.png'),
-            backgroundColor: Colors.grey[200],
-          ),
-        if (!isUser) SizedBox(width: 8),
-        Expanded(
-          child: Container(
-            alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
-            decoration: BoxDecoration(
-              color: isUser ? Color(0xff99EDE9) : Color(0xff9196CD),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  messageText,
-                  style: TextStyle(color: Colors.white),
-                ),
-                if (!isUser)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.thumb_up, size: 16, color: Color(0xffEAEBF6)),
-                        onPressed: () {
-                          print('Liked message: $messageText');
-                        },
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          resendMessage(messageText);
-                        },
-                        child: Icon(
-                          Icons.refresh,
-                          size: 16,
-                          color: Color(0xffEAEBF6),
-                        ),
-                      ),
-                      SizedBox(width: 4), 
-                      IconButton(
-                        icon: Icon(Icons.copy, size: 16, color: Color(0xffEAEBF6)),
-                        onPressed: () {
-                          Clipboard.setData(ClipboardData(text: messageText));
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Copied to clipboard'),
-                              backgroundColor: Color.fromARGB(255, 27, 76, 117),
-                            ),
-                          );
-                          print('Copied message: $messageText');
-                        },
-                      ),
-                    ],
-                  ),
-              ],
-            ),
-          ),
-        ),
-        if (isUser) SizedBox(width: 8),
-        if (isUser)
-          CircleAvatar(
-            backgroundImage: AssetImage('assets/images/profile.jpg'),
-            backgroundColor: Colors.grey[200],
-          ),
-      ],
+  void _sendMessage(String text) async {
+    setState(() {
+      messages.add("You: $text");
+    });
+    final response = await api.post(
+      "http://192.168.1.2:5000/chatbot/ask",
+      data: {"message": text},
+      headers: {},
     );
+    setState(() {
+      messages.add("Bot: ${response['response']}");
+    });
+    controller.clear();
+    _saveMessages();
   }
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Cardi.isDarkMode.value?Color(0xff141218):Colors.white,
-      key: _scaffoldKey,
-      drawer: Drawer(
-        child: SafeArea(
-          child: Column(
-            children: [
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Stack(
+      resizeToAvoidBottomInset: true,
+      body: Column(
         children: [
-          SafeArea(
-            child: Column(
+          const SizedBox(height: 30,),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                final isSender = messages[index].startsWith("You:");
+                return MessageBubble(
+                  text: messages[index].substring(5), // remove "You: " or "Bot: "
+                  isSender: isSender,
+                );
+              },
+            ),
+          ),
+          Container(
+            height: 50,
+            width: width * 0.95,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(
+                width: 2,
+                color: Cardi.isDarkMode.value
+                    ? CupertinoColors.white.withOpacity(0.5)
+                    : CupertinoColors.black.withOpacity(0.5),
+              ),
+            ),
+            padding: const EdgeInsets.only(left: 10),
+            child: Row(
               children: [
+                const SizedBox(width: 10),
                 Expanded(
-                  child: CustomScrollView(
-                    slivers: [
-                      SliverPadding(
-                        padding: EdgeInsets.only(right: 10, left: 10, top: 50),
-                        sliver: SliverList(
-                          delegate: SliverChildBuilderDelegate(
-                                (context, index) => buildMessage(messages[index], screenWidth),
-                            childCount: messages.length,
-                          ),
-                        ),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Send a message',
+                      hintStyle: TextStyle(
+                        color: Cardi.isDarkMode.value
+                            ? CupertinoColors.white.withOpacity(0.5)
+                            : CupertinoColors.black.withOpacity(0.5),
                       ),
-                    ],
+                    ),
                   ),
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: screenWidth * 0.95,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    border: Border.all(width: 2, color: Color(0xff00D3C7)),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_circle_up_outlined,
+                    color: Cardi.isDarkMode.value
+                        ? CupertinoColors.white.withOpacity(0.5)
+                        : CupertinoColors.black.withOpacity(0.5),
                   ),
-                  margin: EdgeInsets.only(bottom: 80, top: 5),
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Row(
-                    children: [
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: TextField(
-                          controller: controller,
-                          decoration: InputDecoration.collapsed(hintText: 'Send a message'),
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.arrow_circle_up_outlined, color: Color(0xff00D3C7)),
-                        onPressed: sendMessage,
-                      ),
-                    ],
-                  ),
+                  onPressed: () {
+                    if (controller.text.isNotEmpty) {
+                      _sendMessage(controller.text);
+                    }
+                  },
                 ),
               ],
             ),
           ),
-          Positioned(
-            top: 30,
-            left: 10,
-            child: FloatingActionButton(
-              mini: true,
-              backgroundColor: Color(0xff00D3C7),
-              onPressed: () {
-                _scaffoldKey.currentState?.openDrawer();
-              },
-              child: Icon(Icons.menu, color: Colors.white),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+        ],
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String text;
+  final bool isSender;
+
+  const MessageBubble({
+    super.key,
+    required this.text,
+    required this.isSender,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Align(
+        alignment: isSender ? Alignment.centerRight : Alignment.centerLeft,
+        child: Container(
+          constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width*0.8),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          decoration: BoxDecoration(
+            color: isSender ? Colors.blueAccent : Colors.grey[300],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: isSender ? Colors.white : Colors.black,
             ),
           ),
-        ],
+        ),
       ),
     );
   }
